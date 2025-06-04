d39ac966bbdc3d62e781dbee7d47ec695968c653
kev1226 <kevin_alt12@hotmail.com>
2025-06-04 00:08:49 -0500

    test deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,11 +11,13 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/cryptos', cryptoRoutes);
 
+const PORT = process.env.PORT || 4000;
+
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Conectado a MongoDB');
-        app.listen(process.env.PORT, () =>
-            console.log(`Servidor backend en puerto ${process.env.PORT}`)
-        );
-    })
-    .catch(err => console.error('Error conectando a MongoDB:', err));
+  .then(() => {
+    console.log('Conectado a MongoDB');
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Servidor backend en puerto ${PORT}`);
+    });
+  })
+  .catch(err => console.error('Error conectando a MongoDB:', err));
